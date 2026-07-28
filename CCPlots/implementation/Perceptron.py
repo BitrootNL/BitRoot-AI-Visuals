c4a03dc8f23d8e1511ee6cea64be52e175635d34
Lina (Selina) Blijleven <12709907+SelinaBlijleven@users.noreply.github.com>
2026-07-28 14:51:02 +0200

    color config applied

--- CCPlots/implementation/Perceptron.py
+++ CCPlots/implementation/Perceptron.py
@@ -31,26 +31,32 @@ class Perceptron(PlotExample):
             ax.set_ylim(0, 10)
             ax.axis('off')
 
+            ann_color = self.resolve_color('annotation')
+            wgt_color = self.resolve_color('weight_label')
+            circle_fill = self.resolve_color('circle_fill')
+            circ_text = self.resolve_color('circle_text')
+            arrow_col = self.resolve_color('arrow')
+
             for i, (x, w, y) in enumerate(zip(inputs, weights, input_positions)):
-                ax.annotate(x, (1, y), fontsize=12, ha='right', color=BITROOT_PALETTE['text'])
-                ax.annotate(w, (2.2, y + 0.4), fontsize=10, ha='left', color=BITROOT_PALETTE['secondary_text'])
+                ax.annotate(x, (1, y), fontsize=12, ha='right', color=ann_color)
+                ax.annotate(w, (2.2, y + 0.4), fontsize=10, ha='left', color=wgt_color)
                 ax.arrow(1.2, y, 2.3, 5 - y, head_width=0.1, head_length=0.2,
-                         fc=BITROOT_PALETTE['text'], ec=BITROOT_PALETTE['text'],
+                         fc=arrow_col, ec=arrow_col,
                          length_includes_head=True)
 
             circle = patches.Circle((4.5, 5), 1.2,
-                                    edgecolor=BITROOT_PALETTE['text'],
-                                    facecolor=BITROOT_PALETTE['secondary'], lw=2)
+                                    edgecolor=ann_color,
+                                    facecolor=circle_fill, lw=2)
             ax.add_patch(circle)
             ax.text(4.5, 5.4, r'$\sum (w_i x_i) + b$', fontsize=12,
-                    ha='center', color=BITROOT_PALETTE['white'])
+                    ha='center', color=circ_text)
             ax.text(4.5, 4.3, labels['activation'], fontsize=10,
-                    ha='center', color=BITROOT_PALETTE['white'])
+                    ha='center', color=circ_text)
 
             ax.arrow(5.7, 5, 2.5, 0, head_width=0.1, head_length=0.2,
-                     fc=BITROOT_PALETTE['text'], ec=BITROOT_PALETTE['text'],
+                     fc=arrow_col, ec=arrow_col,
                      length_includes_head=True)
-            ax.text(8.5, 5, labels['output'], fontsize=12, ha='left', color=BITROOT_PALETTE['text'])
+            ax.text(8.5, 5, labels['output'], fontsize=12, ha='left', color=ann_color)
 
             self.save_figure(fig, "default", suffix=suffix)
 
